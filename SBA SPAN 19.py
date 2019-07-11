c837cf9a66183ed15aeb8d67fe3d39b0d2157ac2
jeanho rodriguez <jeanho.rodriguez@gmail.com>
2019-07-11 12:08:15 -0600

    Test Version Control

--- SBA SPAN 19.py
+++ SBA SPAN 19.py
@@ -1,4 +1,4 @@
-#SBA SPAN - 2019.
+﻿#SBA SPAN - 2019.
 #Jeanho Rodriguez.
 #Date: 6/28/2019.
 
@@ -472,4 +472,4 @@ df_soap_merge_final['Sort_Code'] = df_soap_merge_final['Sort_Code'].map(sort_cod
 
 
 #df_soap_merge_final.to_csv("DF_SOAP_SBA_SPAN_FINAL.csv", sep=',', encoding='utf-8', index = False)
-
+# Testing VERSION CONTROL!
